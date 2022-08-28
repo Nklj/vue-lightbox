0dae0beb75931e791c5e2be3df0b48a09ad16fa0
Nklj <vlkvnklj@gmail.com>
2022-08-28 19:16:05 +0300

    finish navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="grey darken-3">
     <div class="nav-wrapper">
-      <ul class="left">
+      <ul class="left" @click.stop="screenChooser">
         <li>
           <input
             type="file"
@@ -10,30 +10,60 @@
             multiple
             webkitdirectory
             directory
+            @change="onFileSelecter"
           />
           <label for="file-input"
             ><i class="material-icons white-text">download</i></label
           >
         </li>
-
-        <li>
-          <a><i class="material-icons">zoom_out_map</i></a>
+        <li :class="{ active: selectedScreen==='fill' }">
+          <a><i class="material-icons" id="fill">zoom_out_map</i></a>
         </li>
-        <li class="active"><a>1:1</a></li>
-        <li>
-          <a><i class="material-icons">swap_horiz</i></a>
+        <li :class="{ active: selectedScreen==='1:1' }">
+          <a><i id="1:1">1:1</i></a>
         </li>
-        <li>
-          <a><i class="material-icons">swap_vert</i></a>
+        <li :class="{ active: selectedScreen==='horiz' }">
+          <a><i class="material-icons" id="horiz">swap_horiz</i></a>
+        </li>
+        <li :class="{ active: selectedScreen==='vert' }">
+          <a><i class="material-icons" id="vert">swap_vert</i></a>
         </li>
       </ul>
-      <a class="right"><i class="material-icons">fullscreen</i></a>
+      <a v-if="!fullScreen" class="right" @click="fullScreen=!fullScreen"><i class="material-icons">fullscreen</i></a>
+      <a v-else class="right" @click="fullScreen=!fullScreen"><i class="material-icons">fullscreen_exit</i></a>
     </div>
   </nav>
+  <div>{{fullScreen}}
+    <div v-if="!selectedFiles.length">Выберите директорию с изображениями</div>
+    <div v-else>{{ selectedFiles[0] }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      selectedFiles: [],
+      selectedScreen: "fill",
+      fullScreen: false,
+    };
+  },
+
+  methods: {
+    onFileSelecter(event) {
+      const onlyImages = [...event.target.files].filter((file) =>
+        /image|pdf/g.test(file.type)
+      );
+      this.selectedFiles = onlyImages;
+    },
+    screenChooser(event) {
+      if (/fill|horiz|vert|1:1/g.test(event.target.id)) {
+        this.selectedScreen = event.target.id;
+      }
+    },
+    
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -49,5 +79,9 @@ input[type="file"] {
 }
 li:first-child {
   margin-right: 40px;
+}
+label,
+a:hover {
+  cursor: pointer;
 }
 </style>
